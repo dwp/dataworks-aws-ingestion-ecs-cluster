68d4c5188d108de03714ff1ddfc1677bf66ee734
danhill <danhill@digital.uc.dwp.gov.uk>
2020-11-18 11:32:37 +0000

    create new ECS cluster with EC2 capacity provider

--- variables.tf
+++ variables.tf
@@ -13,3 +13,29 @@ variable "region" {
   type    = string
   default = "eu-west-2"
 }
+
+variable "ingestion_ecs_cluster_asg_max" {
+  description = "Max ingestion asg size"
+  default = {
+    development = 9
+    qa          = 9
+    integration = 9
+    preprod     = 9
+    production  = 9
+  }
+}
+
+variable "ingestion_ecs_cluster_ec2_size" {
+  default = {
+    development = "t3.medium"
+    qa          = "t3.medium"
+    integration = "t3.medium"
+    preprod     = "t3.medium"
+    production  = "m5.large"
+  }
+}
+
+variable "ecs_hardened_ami_id" {
+  description = "The AMI ID of the latest/pinned ECS Hardened AMI Image"
+  type        = string
+}
