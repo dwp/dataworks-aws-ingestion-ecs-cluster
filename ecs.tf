0f03913e29cc0391a450872a2ba886cc88a509c4
danhill <danhill@digital.uc.dwp.gov.uk>
2020-11-23 15:42:15 +0000

    use ASG specific tag resource in dynamic block
    Signed-off-by: danhill <danhill@digital.uc.dwp.gov.uk>

--- ecs.tf
+++ ecs.tf
@@ -75,7 +75,15 @@ resource "aws_autoscaling_group" "ingestion_ecs_cluster" {
     ignore_changes        = [desired_capacity]
   }
 
-  tags = [local.ingestion_ecs_asg_tags]
+  dynamic "tag" {
+    for_each = local.ingestion_ecs_asg_tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_launch_template" "ingestion_ecs_cluster" {
