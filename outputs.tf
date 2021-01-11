e41199cd0d1b0ddab8623bdd7dae2869197d95eb
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-01-11 15:45:18 +0000

    Add an output for the ASG

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
-
 output "ingestion_ecs_cluster" {
   value = aws_ecs_cluster.ingestion_ecs_cluster
+}
+
+output "ingestion_ecs_cluster_autoscaling_group" {
+  value = aws_autoscaling_group.ingestion_ecs_cluster
 }
 
 output "ingestion_ecs_cluster_security_group" {
