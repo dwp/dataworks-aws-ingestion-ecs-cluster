eb30eac4e5b7bea573096ef70b668c29ee08eb5d
jonnym <jon.melia@cloudadept.co.uk>
2023-06-15 11:37:12 +0100

    refactor: Trend/Tanium config (#33)
    Co-authored-by: Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>

--- security_groups.tf
+++ security_groups.tf
@@ -68,3 +68,43 @@ resource "aws_security_group_rule" "ingestion_ecs_to_s3_http" {
   to_port           = 80
   security_group_id = aws_security_group.ingestion_ecs_cluster.id
 }
+
+resource "aws_security_group_rule" "ingestion_ecs_host_outbound_tanium_1" {
+  description       = "Ingestion ECS host outbound port 1 to Tanium"
+  type              = "egress"
+  from_port         = var.tanium_port_1
+  to_port           = var.tanium_port_1
+  protocol          = "tcp"
+  prefix_list_ids   = local.tanium_prefix[local.environment]
+  security_group_id = aws_security_group.ingestion_ecs_cluster.id
+}
+
+resource "aws_security_group_rule" "ingestion_ecs_host_outbound_tanium_2" {
+  description       = "Ingestion ECS host outbound port 2 to Tanium"
+  type              = "egress"
+  from_port         = var.tanium_port_2
+  to_port           = var.tanium_port_2
+  protocol          = "tcp"
+  prefix_list_ids   = local.tanium_prefix[local.environment]
+  security_group_id = aws_security_group.ingestion_ecs_cluster.id
+}
+
+resource "aws_security_group_rule" "ingestion_ecs_host_inbound_tanium_1" {
+  description       = "Ingestion ECS host inbound port 1 from Tanium"
+  type              = "ingress"
+  from_port         = var.tanium_port_1
+  to_port           = var.tanium_port_1
+  protocol          = "tcp"
+  prefix_list_ids   = local.tanium_prefix[local.environment]
+  security_group_id = aws_security_group.ingestion_ecs_cluster.id
+}
+
+resource "aws_security_group_rule" "ingestion_ecs_host_inbound_tanium_2" {
+  description       = "Ingestion ECS host inbound port 2 from Tanium"
+  type              = "ingress"
+  from_port         = var.tanium_port_2
+  to_port           = var.tanium_port_2
+  protocol          = "tcp"
+  prefix_list_ids   = local.tanium_prefix[local.environment]
+  security_group_id = aws_security_group.ingestion_ecs_cluster.id
+}
--- userdata.tpl
+++ userdata.tpl
@@ -37,7 +37,10 @@ chmod u+x /opt/ingestion/cloudwatch.sh
 
 echo "Setup hcs pre-requisites"
 chmod u+x /opt/ingestion/config_hcs.sh
-/opt/ingestion/config_hcs.sh "${hcs_environment}" "${proxy_host}" "${proxy_port}"
+/opt/ingestion/config_hcs.sh "${hcs_environment}" "${proxy_host}" "${proxy_port}" \
+    "${tanium_server_1}" "${tanium_server_2}" "${tanium_env}" "${tanium_port}" \
+    "${tanium_log_level}" "${install_tenable}" "${install_trend}" "${install_tanium}" \
+    "${tenantid}" "${token}" "${policyid}" "${tenant}"
 
 echo "Creating ingestion user"
 useradd ingestion -m
